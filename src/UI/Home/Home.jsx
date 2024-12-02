bf948d67635c2b12c9619485a1fb0dc9b7a1fbf7
Lucky-Bhure <luckybhure19@gmail.com>
2024-12-02 14:59:59 +0530

    new commit

--- src/UI/Home/Home.jsx
+++ src/UI/Home/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div style={{height: "90vh", display:"flex", justifyContent:"center", alignItems: "center", flexDirection:"column"}}>
       <h1>Home Page</h1>
-      <p style={{fontSize:"larger", marginTop: "1rem"}}>Go to Job page By click on <a href="/jobs">Jobs</a> in Navbar</p>
+      <p style={{fontSize:"larger", marginTop: "1rem"}}>Go to Job page By click on Jobs in Navbar</p>
     </div>
   )
 }
