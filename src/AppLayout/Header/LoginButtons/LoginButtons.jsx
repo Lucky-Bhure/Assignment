7c0e7b4bea6b0376737bfd74a80db8a2bf16b320
Lucky-Bhure <luckybhure19@gmail.com>
2024-12-02 15:07:42 +0530

    new commit

--- src/AppLayout/Header/LoginButtons/LoginButtons.jsx
+++ src/AppLayout/Header/LoginButtons/LoginButtons.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./LoginButtons.css"
+import { NavLink } from 'react-router-dom'
 
 const LoginButtons = () => {
   return (
     <>
-      <button className='jobseeker btn'>Jobseeker Login</button>
-      <button className='employer btn'>Employer login</button>
+      <NavLink to={"/jobseekerlogin"} className='jobseeker btn'>Jobseeker Login</NavLink>
+      <NavLink to={"employerlogin"} className='employer btn'>Employer login</NavLink>
     </>
   )
 }
